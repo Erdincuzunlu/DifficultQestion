c1e587fb244e4d32be03e16c17cc872754a1a2d2
Erdinç uzunlu <134061490+Erdincuzunlu@users.noreply.github.com>
2024-08-28 20:44:34 +0200

    Add files via upload

---
+++ interviewquestion.py
@@ -0,0 +1,11 @@
+#before : hello
+#after : HeLlO
+
+def alternating(string):
+    new_string = ""
+    for string_index in range(len(string)):
+        if string_index % 2 == 0:
+            new_string += string[string_index].upper()
+        else:
+            new_string += string[string_index].lower()
+    print(new_string)
